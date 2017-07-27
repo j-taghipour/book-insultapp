9f75629624c3abdd716f48c9f0e82d5676a423b3
jafar <t_jafar@yahoo.com>
2017-07-27 15:41:01 +0430

    Adding a REST endpoint

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,11 @@
+package org.openshift;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by jafar on 7/27/17.
+ */
+@ApplicationPath("/api")
+public class JaxrsConfig extends Application {
+}
